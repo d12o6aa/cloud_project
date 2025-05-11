2ef07583de4f2137d1ea8d14e1ab861331574546
d12o6aa <doaakarem798@gmail.com>
2025-05-11 03:19:58 +0300

    updated V 1.0.0

--- freelance_app/run.py
+++ freelance_app/run.py
@@ -27,4 +27,5 @@ app.register_blueprint(freelance_bp, url_prefix="/freelancer")
 
 app.register_blueprint(main_bp)
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
